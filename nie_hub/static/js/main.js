e4f5bf1dcfcb61a4cfba8472ee3b1c2b9fd60678
vrushank <iamvrushank@gmail.com>
2018-10-10 14:08:30 +0530

    Added home page

---
+++ nie_hub/static/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$(".menu_item").click(function(event) {
+		$(".menu_item").removeClass('active');
+	});
+});
--- nie_hub/views.py
+++ nie_hub/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	return render(request,'nie_hub/home.html',{})
+
+def login(request):
+	return render(request,'nie_hub/login.html',{})	
+
+def signup(request):
+	return render(request,'nie_hub/signup.html',{})
